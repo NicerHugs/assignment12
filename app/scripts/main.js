0f38ae714d9f2a44b52f76d4869f9bd570810e13
Jess Scheuring <jscheuring@gmail.com>
2014-09-16 14:02:05 -0400

    implemented issues list

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,18 @@
+function renderTemplate(templateID, location, dataModel) {
+    var templateString = $(templateID).text();
+    var templateFunction = _.template(templateString);
+    var renderedTemplate = templateFunction(dataModel);
+    $(location).append(renderedTemplate);
+}
+
 $.ajax({
     url: "https://api.github.com/issues",
     type: 'get'})
     .done(function(data){
-        console.log(data);
+        _.each(data, function(datum) {
+            var issuesModel = {
+                title: datum.title
+            };
+            renderTemplate('#templates-issues-list', '.issues-list', issuesModel);
+        });
     });
