21f4fa93b7112d3b7d5be27ccae4308814c56f4f
Jess Scheuring <jscheuring@gmail.com>
2014-09-16 15:51:58 -0400

    implemented details section with comments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,28 +11,45 @@ $.ajax({
     // data: {state: 'all'}
     })
     .done(function(data){
-        var issuesModel = _.map(data, function(datum) {
-            return {
-                    title: datum.title,
-                    details: datum.body,
-                    commentsURL: datum.comments_url
-            };
-        });
-        _.each(issuesModel, function(issueModel) {
-            renderTemplate('#templates-issues-list', '.issues-list', issueModel);
-            commentsGetRequest(issueModel.commentsURL);
-        });
+        buildIssuesList(data);
     });
 
-
-function commentsGetRequest(url) {
-
-$.ajax({
-    url: url,
-    type: 'get',
-    // data: {state: 'all'}
-    })
-    .done(function(data){
-        console.log(data);
+function buildIssuesList(data) {
+    var issuesModel = _.map(data, function(datum) {
+        return {
+                title: datum.title,
+                description: datum.body,
+                commentsURL: datum.comments_url
+        };
+    });
+    $('.issue').remove();
+    _.each(issuesModel, function(issueModel) {
+        renderTemplate('#templates-issues-list', '.issues-list', issueModel);
     });
 }
+
+$(document).on('click', '.issue-link', function(e) {
+    e.preventDefault();
+    var url = $(this).attr('id');
+    var title = $('h2', this).text();
+    var description = $('p', this).text();
+    $.ajax({
+        url: url,
+        type: 'get',
+    })
+    .done(function(data){
+        var issueDetailsModel = {
+            title: title,
+            description: description
+        };
+        renderTemplate('#templates-issue-details', '.issue-details', issueDetailsModel);
+        var commentsModel = _.map(data, function(datum) {
+            return {
+              comment: datum.body
+            };
+        });
+        _.each(commentsModel, function(commentModel) {
+            renderTemplate('#templates-issue-comments', '.issue-comments', commentModel);
+        });
+    });
+});
